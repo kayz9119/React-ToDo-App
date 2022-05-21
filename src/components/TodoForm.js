0efa3c0f93566135fa867ae640eaad55d62e11ea
Kerem Arslantürk <solanahtarigibiyim@gmail.com>
2022-05-22 02:09:24 +0300

    Created a form for add todos.

---
+++ src/components/TodoForm.js
@@ -0,0 +1,28 @@
+import {useState} from "react";
+
+function TodoForm(props) {
+    const [input, setInput] = useState('');
+    const handleSubmit = e => {
+        e.preventDefault();
+        props.onSubmit({
+            id: Math.floor(Math.random() * 9999),
+            text: input
+        })
+        setInput('');
+    }
+
+    const handleChange = (e) => {
+        setInput(e.target.value)
+    }
+
+    return(
+        <>
+            <form className="todoForm" onSubmit={handleSubmit}>
+                <input type="text" placeholder="Write some todo" value={input} className="todoInput" name="text" onChange={handleChange} />
+                <button className="todoButton">Add a Todo!</button>
+            </form>
+        </>
+    );
+}
+
+export default TodoForm;
