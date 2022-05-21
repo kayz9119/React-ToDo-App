3087763fa37fb04428ac56f4c7de19b6859eac61
Kerem Arslantürk <solanahtarigibiyim@gmail.com>
2022-05-22 02:10:07 +0300

    added my two components.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import TodoForm from "./components/TodoForm";
+import { useState } from "react";
+import Todo from "./components/Todo";
 function App() {
+    const [todos, setTodos] = useState([]);
+    const addTodo = todo => {
+        if(!todo.text || /^\s$/.test(todo.text)) {
+            return
+        }
+        const newTodos = [...todos, todo]
+        setTodos(newTodos)
+    }
   return (
     <>
-      <p>Deneme</p>
+        <TodoForm onSubmit={addTodo} />
+        <Todo todos={todos}/>
     </>
   );
 }
